388bd3ddbfe9aa833b463700732cee11da623faa
Филинов Анатолий Григорьевич <AGFilinov@sberbank.ru>
2023-06-01 18:18:13 +0300

    @EnableAspectJAutoProxy(proxyTargetClass = true) (GCLIB enable)

--- src/main/java/com/interview/carwash/config/AppConfig.java
+++ src/main/java/com/interview/carwash/config/AppConfig.java
@@ -9,12 +9,14 @@ import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 import java.util.stream.Collectors;
 
 import static org.modelmapper.convention.MatchingStrategies.STRICT;
 
 @Configuration
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class AppConfig {
 
     @Bean
